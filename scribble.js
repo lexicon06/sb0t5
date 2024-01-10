5f55df0a6a00856c02e7415ad8603d8ec0ad94ce
lexicon06 <pablo.hehe2@gmail.com>
2024-01-10 08:58:32 -0300

    scribble.js has been created

---
+++ scribble.js
@@ -0,0 +1,31 @@
+print(0, "scribble.js has been loaded correctly");
+
+var Commands = ["scribble", "foto"];
+var LevelScribble = 0;
+
+function onCommand(userobj, command, target, args) {
+  if (userobj.level >= LevelScribble) {
+    for (var i = 0; i < Commands.length; i++) {
+      if (command.toLowerCase().substring(0, Commands[i].length) == Commands[i]) {
+        var fotoScribble = new Scribble();
+        fotoScribble.src = command.substring(Commands[i].length + 1);
+        fotoScribble.oncomplete = handleScribble;
+        fotoScribble.download("\x0314Scribble sent by \x06\x07" + userobj.name);
+      }
+    }
+  }
+}
+
+function handleScribble(e) {
+  if (e) {
+    var Image = this;
+    Users.local(function (i) {
+      i.scribble(Image);
+    });
+    print(this.arg);
+  } else {
+    print(0, "\x0314Sorry bad scribble response.");
+  }
+}
+
+//made by lexicon06 for sb0t5
